6e2ada2952eb47d5f18b189c6f8e6356a380dfc5
2av - Miroslav Milev <mm@2av.de>
2021-09-15 13:30:33 +0300

    steps (prev, next button)

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,37 +1,40 @@
 <template>
-    <div>
-        <h1 class="text-indigo-">{{ store.state.surveys.survey.name }}</h1>
-        <div
-            v-for="(step, index) in store.state.surveys.surveySteps"
-            :key="'step-element-' + index"
-        >
-            {{ step }}
-
-            <SurveyElementBinaryQuestion
-                v-if="1 < 0"
-            ></SurveyElementBinaryQuestion>
-            <SurveyElementMultipleChoice
-                v-if="1 < 0"
-            ></SurveyElementMultipleChoice>
-            <SurveyElementStarRating v-if="1 < 0"></SurveyElementStarRating>
-            <SurveyElementSimpleText v-if="1 < 0"></SurveyElementSimpleText>
-
-            <div v-if="step.name === 'Video'">
-                <SurveyElementVideo :content="step"></SurveyElementVideo>
-            </div>
+    <div class="main-page">
+        <div class="survey-steps">
+            <h1 class="text-indigo-">{{ store.state.surveys.survey.name }}</h1>
+        </div>
+        <SurveyElementBinaryQuestion v-if="1 < 0"></SurveyElementBinaryQuestion>
+        <SurveyElementMultipleChoice v-if="1 < 0"></SurveyElementMultipleChoice>
+        <SurveyElementStarRating v-if="1 < 0"></SurveyElementStarRating>
+        <SurveyElementSimpleText v-if="1 < 0"></SurveyElementSimpleText>
+        {{ store.state.surveys.surveySteps[page] }}
+        {{ store.state.surveys.surveySteps[page].name }}
+        {{ page }}
+        <div v-if="store.state.surveys.surveySteps[page].name === 'Video'">
+            <SurveyElementVideo
+                :content="store.state.surveys.surveySteps[page]"
+            ></SurveyElementVideo>
+        </div>
+        <div class="survey-navigation">
+            <SurveyNavigation
+                @next-step="nextStep()"
+                @prev-step="prevStep()"
+            ></SurveyNavigation>
         </div>
     </div>
 </template>
 
 <script>
 import { useStore } from 'vuex'
-import { onMounted } from 'vue'
 
 import SurveyElementBinaryQuestion from './elements/SurveyElementBinaryQuestion.vue'
 import SurveyElementMultipleChoice from './elements/SurveyElementMultipleChoice.vue'
 import SurveyElementStarRating from './elements/SurveyElementStarRating.vue'
 import SurveyElementSimpleText from './elements/SurveyElementSimpleText.vue'
 import SurveyElementVideo from './elements/SurveyElementVideo.vue'
+import SurveyNavigation from './SurveyNavigation.vue'
+import { ref } from '@vue/reactivity'
+
 export default {
     name: 'Home',
     components: {
@@ -40,22 +43,28 @@ export default {
         SurveyElementStarRating,
         SurveyElementSimpleText,
         SurveyElementVideo,
+        SurveyNavigation,
     },
     setup() {
+        const page = ref(0)
         const store = useStore()
         store.dispatch('surveys/getSurvey', 1)
         store.dispatch('surveys/getSurveySteps', 1)
 
-        const method1 = () => {
-            console.log('method1')
+        const nextStep = () => {
+            console.log('nextStep')
+            page.value++
+        }
+        const prevStep = () => {
+            console.log('prevStep')
+            page.value--
         }
 
-        onMounted(() => {
-            method1()
-        })
-
         return {
+            page,
             store,
+            nextStep,
+            prevStep,
         }
     },
 }
---
+++ src/components/SurveyNavigation.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="survey-navigation-inner">
+        <button class="prev nav-button p-2" @click="prevStep()">prev</button>
+        <button class="next nav-button p-2" @click="nextStep()">next</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navigation',
+    emits: ['next-step', 'prev-step'],
+    setup(props, { emit }) {
+        const nextStep = () => {
+            console.log('nextStep')
+            emit('next-step')
+        }
+        const prevStep = () => {
+            console.log('prevStep')
+            emit('prev-step')
+        }
+
+        return {
+            nextStep,
+            prevStep,
+        }
+    },
+}
+</script>
+
+<style scoped>
+.survey-navigation-inner {
+    width: 200px;
+    left: 50%;
+    transform: translateX(-50%);
+    position: relative;
+}
+
+.nav-button {
+    position: absolute;
+    background-color: gray;
+}
+
+.nav-button.prev {
+    left: 0px;
+}
+
+.nav-button.next {
+    right: 0px;
+}
+</style>
