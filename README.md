ae6e78503d65f39ac5958fe0862490e763bf836b
Thomas Geissl <tg@2av.de>
2022-01-27 11:20:04 +0100

    added electron support

---
+++ README.md
@@ -0,0 +1,8 @@
+# evaluation-tool-survey-player
+
+yarn install
+yarn dev
+yarn build
+yarn electron:dev
+yarn electron:build
+
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import pluginEnv from 'vite-plugin-vue-env'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: process.env.ELECTRON=="true" ? './' : ".",
     plugins: [vue(), pluginEnv()],
     server: {
         port: 3001,
