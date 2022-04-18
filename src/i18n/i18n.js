3271d8fd5223f3322b98579ce2faee32f46333a7
2av - Martin Schmitt <ms@2av.de>
2022-04-18 18:29:44 +0200

    - silenced i18n fallback warnings

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -2,7 +2,11 @@ import { createI18n } from 'vue-i18n'
 import messages from './localizations'
 
 export default createI18n({
-    locale: 'de',
     messages,
+    locale: 'de',
     fallbackLocale: 'de',
+    silentTranslationWarn: true,
+    silentFallbackWarn: true,
+    missingWarn: false,
+    fallbackWarn: false,
 })
